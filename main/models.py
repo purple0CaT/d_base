77e4d9508b9a28868b150e29bdff1c2eb9b0637d
Istvan Sandor <steven.ford.j@gmail.com>
2023-06-19 22:20:02 +0100

    added todo list

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    ACTION_CHOICES = (
+        ('done', 'Done'),
+        ('none', 'None'),
+    )
+
+    name = models.CharField(max_length=255)
+    action = models.CharField(max_length=4, choices=ACTION_CHOICES, default='none')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
--- main/views.py
+++ main/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
+import json
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    homePage = 'home.html'
+    context = {'csrf_token': request.META['CSRF_COOKIE'], 'todos': [], 'error': None}
+    # Create
+    if(request.method == 'POST'):
+        form = TodoForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            context['error'] = 'Invalid form'
+            return render(request,homePage,context)
+    # Update
+    elif(request.method == 'PUT'):
+        json_data = json.loads(request.body)
+        todo = Todo.objects.get(id=json_data['todo_id'])
+        if todo.action == 'none':
+            todo.action = 'done'
+        else:
+            todo.action = 'none'
+        todo.save()
+
+        context['todos'] = Todo.objects.order_by('created_at')
+        return render(request,homePage,context)   
+    # Delete
+    elif(request.method == 'DELETE'):
+        json_data = json.loads(request.body)
+        todo = Todo.objects.get(id=json_data['todo_id'])
+        todo.delete()
+
+        context['todos'] = Todo.objects.order_by('created_at')
+        return render(request,homePage,context)    # Read
+    else:
+        context['todos'] = Todo.objects.order_by('created_at')
+        return render(request,homePage,context)
 
 def about(request):
     return render(request,'about.html')
