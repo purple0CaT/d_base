d70cfa595dd9eb8f8a44bc8038e43ba1a258d188
Istvan Sandor <steven.ford.j@gmail.com>
2023-06-21 22:14:38 +0100

    add model

--- main/models.py
+++ main/models.py
@@ -6,9 +6,17 @@ class Todo(models.Model):
         ('done', 'Done'),
         ('none', 'None'),
     )
-
     name = models.CharField(max_length=255)
     action = models.CharField(max_length=4, choices=ACTION_CHOICES, default='none')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class Feedback(models.Model):
+    title = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    message = models.TextField()
+    user_type = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
