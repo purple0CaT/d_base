e3ff6f529fd109769bc492a722fb52d626a5537f
Istvan Sandor <steven.ford.j@gmail.com>
2023-06-25 14:05:10 +0100

    raw query

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.dashboard_view, name='custom_admin'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('raw_query/', views.raw_query, name='raw_query'),
 ]
--- custom_admin/views.py
+++ custom_admin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from .utils import check_if_admin
+from django.db import connection
 
 # Create your views here.
 
@@ -27,7 +28,29 @@ def dashboard_view(request):
 
 
 
-@login_required(login_url="/custom_admin/login/")
+@login_required(login_url="login/")
 def logout_view(request):
     logout(request)
     return redirect('/custom_admin/login')
+
+
+@login_required(login_url="login/")
+def raw_query(request):
+    context={'todos':[]}
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM main_todo")
+        rows = cursor.fetchall()
+        # Process the query results
+        todos = []
+        for row in rows:
+            # Create a dictionary or an object to represent the todo
+            todo = {
+                'id': row[0],
+                'name': row[1],
+                'action': row[2],
+                # ... Add other attributes as needed
+            }
+            todos.append(todo)
+        context['todos']=todos
+
+    return render(request,'dash.html',context)
